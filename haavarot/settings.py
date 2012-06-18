9f8005c83e40573686623d923836f88de487e339
alonisser <alonisser@gmail.com>
2012-06-18 22:06:55 +0300

    added bootstrap toolkit

--- haavarot/settings.py
+++ haavarot/settings.py
@@ -130,7 +130,8 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
-    'transfers'
+    'transfers',
+    'bootstrap_toolkit',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
